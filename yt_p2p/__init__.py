c9c45b77215522b80a7bd5d4712e2a33fb6244a6
Britton Smith <brittonsmith@gmail.com>
2018-09-06 22:22:36 +0100

    Initial import.

---
+++ yt_p2p/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pop2Prime project analysis tools.
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) Britton Smith <brittonsmith@gmail.com>.  All rights reserved.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------

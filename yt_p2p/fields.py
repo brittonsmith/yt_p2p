9f5fa54e25cccc213a5bcd397eb757662bb8f78b
Britton Smith <brittonsmith@gmail.com>
2018-09-13 01:01:47 +0100

    Adding more fields and halo callbacks.

--- yt_p2p/fields.py
+++ yt_p2p/fields.py
@@ -13,9 +13,71 @@ Pop2Prime fields.
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import numpy as np
+
 def _metallicity3(field, data):
     return data["enzo", "SN_Colour"] / data["gas", "density"]
 
+def _metal3_mass(field, data):
+    return data["enzo", "SN_Colour"] * data["index", "cell_volume"]
+
+def _metallicity3_min7(field, data):
+    field_data = data["enzo", "SN_Colour"] / data["gas", "density"]
+    field_data.convert_to_units("")
+    min_Z = data.ds.quan(1.e-7, "Zsun").in_units("")
+    field_data[field_data < min_Z] = 0.5 * min_Z
+    return field_data
+
+def _tangential_velocity_magnitude(field, data):
+    return np.sqrt(data["gas", "velocity_spherical_theta"]**2 +
+                   data["gas", "velocity_spherical_phi"]**2)
+
+def _HD_H2(field, data):
+    return data["gas", "HD_density"] / data["gas", "H2_density"]
+
+def _dark_matter_mass(field, data):
+    return data["gas", "dark_matter_density"] * data["index", "cell_volume"]
+
+def _vortical_time(field, data):
+    return 1. / data["gas", "vorticity_magnitude"]
+
+def _vortical_dynamical_ratio(field, data):
+    return data["gas", "vortical_time"] / data["gas", "dynamical_time"]
+
+def _vortical_cooling_ratio(field, data):
+    return data["gas", "vortical_time"] / data["gas", "cooling_time"]
+
+def _cooling_dynamical_ratio(field, data):
+    return data["gas", "cooling_time"] / data["gas", "dynamical_time"]
+
 def add_p2p_fields(ds):
-    ds.add_field("metallicity3", function=_metallicity3,
+    ds.add_field("metallicity3",
+                 function=_metallicity3,
                  units="Zsun", sampling_type="cell")
+    ds.add_field(("gas", "metal3_mass"),
+                 function=_metal3_mass,
+                 units="g", sampling_type="cell")
+    ds.add_field(("gas", "metallicity3_min7"),
+                 function=_metallicity3_min7,
+                 units="Zsun", sampling_type="cell")
+    ds.add_field(("gas", "vortical_time"),
+                 function=_vortical_time,
+                 units="s", sampling_type="cell")
+    ds.add_field(("gas", "dark_matter_mass"),
+                 function=_dark_matter_mass,
+                 units="g", sampling_type="cell")
+    ds.add_field(("gas", "vortical_dynamical_ratio"),
+                 function=_vortical_dynamical_ratio,
+                 units="", sampling_type="cell")
+    ds.add_field(("gas", "vortical_cooling_ratio"),
+                 function=_vortical_cooling_ratio,
+                 units="", sampling_type="cell")
+    ds.add_field(("gas", "cooling_dynamical_ratio"),
+                 function=_cooling_dynamical_ratio,
+                 units="", sampling_type="cell")
+    ds.add_field(("gas", "HD_H2_ratio"),
+                 function=_HD_H2,
+                 units="", sampling_type="cell")
+    ds.add_field(("gas", "tangential_velocity_magnitude"),
+                 function=_tangential_velocity_magnitude,
+                 units="cm/s", sampling_type="cell")
--- yt_p2p/halo_catalog_callbacks.py
+++ yt_p2p/halo_catalog_callbacks.py
@@ -13,11 +13,14 @@ HaloCatalog callbacks
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import numpy as np
 import os
 import yt
 
 from yt.extensions.astro_analysis.halo_analysis.api import \
     add_callback
+from yt.extensions.astro_analysis.halo_analysis.halo_callbacks import \
+    periodic_distance
 
 def sphere_projection(halo, fields, weight_field=None, axes="xyz", output_dir="."):
     yt.mylog.info("Projecting halo %d." % halo.quantities["particle_identifier"])
@@ -31,3 +34,88 @@ def sphere_projection(halo, fields, weight_field=None, axes="xyz", output_dir=".
         plot.save(os.path.join(halo.halo_catalog.output_dir, output_dir,
                                "halo_%06d" % (halo.quantities['particle_identifier'])))
 add_callback("sphere_projection", sphere_projection)
+
+def halo_data_radius(halo, radius_field="virial_radius"):
+    dds = halo.halo_catalog.data_ds
+    return dds.quan(halo.quantities[radius_field].to("unitary"))
+
+def halo_data_center(halo):
+    dds = halo.halo_catalog.data_ds
+    return dds.arr(
+        [halo.quantities["particle_position_%s" % axis].to("unitary")
+         for axis in "xyz"])
+
+def set_halo_center(halo, new_center):
+    for i, axis in enumerate("xyz"):
+        halo.quantities["particle_position_%s" % axis] = new_center[i]
+
+def get_my_sphere(halo, radius_field="virial_radius"):
+    dds = halo.halo_catalog.data_ds
+    sphere = getattr(halo, "data_object", None)
+    if sphere is None:
+        center_orig = halo_data_center(halo)
+        radius_orig = halo_data_radius(halo, radius_field="virial_radius")
+        sphere = dds.sphere(center_orig, radius_orig)
+    return sphere
+
+def my_sphere(halo, radius):
+    dds = halo.halo_catalog.data_ds
+    center = halo_data_center(halo)
+    sphere = dds.sphere(center, radius)
+    halo.data_object = sphere
+add_callback("my_sphere", my_sphere)
+
+def set_inner_bulk_velocity(halo, inner_radius):
+    dds = halo.halo_catalog.data_ds
+    center = halo_data_center(halo)
+    sphere = dds.sphere(center, inner_radius)
+    bulk_velocity = sphere.quantities.bulk_velocity()
+    yt.mylog.info("Halo %06d: Setting bulk velocity to %s.",
+                  halo.quantities["particle_identifier"], bulk_velocity)
+    halo.data_object.set_field_parameter("bulk_velocity", bulk_velocity)
+add_callback("set_inner_bulk_velocity", set_inner_bulk_velocity)
+
+def set_field_max_bulk_velocity(halo, field, radius_field="virial_radius"):
+    dds = halo.halo_catalog.data_ds
+    sphere = get_my_sphere(halo, radius_field="virial_radius")
+
+    max_vals = sphere.quantities.sample_at_max_field_values(
+        field, ['velocity_%s' % ax for ax in "xyz"])
+    bulk_velocity = dds.arr(max_vals[1:])
+    yt.mylog.info("Halo %06d: Setting bulk velocity to %s.",
+                  halo.quantities["particle_identifier"], bulk_velocity)
+    halo.data_object.set_field_parameter("bulk_velocity", bulk_velocity)
+    halo.data_object.clear_data()
+add_callback("set_field_max_bulk_velocity", set_field_max_bulk_velocity)
+
+def set_inner_angular_momentum_vector(halo, inner_radius):
+    dds = halo.halo_catalog.data_ds
+    center = halo_data_center(halo)
+    sphere = dds.sphere(center, inner_radius)
+    angular_momentum_vector = sphere.quantities.angular_momentum_vector()
+    normal = angular_momentum_vector / np.sqrt((angular_momentum_vector**2).sum())
+    yt.mylog.info("Halo %06d: Setting angular momentum vector to %s.",
+                  halo.quantities["particle_identifier"], normal)
+    halo.data_object.set_field_parameter("normal", normal)
+    halo.data_object.clear_data()
+add_callback("set_inner_angular_momentum_vector", set_inner_angular_momentum_vector)
+
+def field_max_center(halo, field, radius_field="virial_radius"):
+    dds = halo.halo_catalog.data_ds
+    center_orig = halo_data_center(halo)
+    sphere = get_my_sphere(halo, radius_field="virial_radius")
+
+    max_vals = sphere.quantities.max_location(field)
+    new_center = dds.arr(max_vals[1:])
+
+    distance = periodic_distance(
+        center_orig.in_units("unitary").v,
+        new_center.in_units("unitary").v)
+    distance = dds.quan(distance, "unitary")
+    yt.mylog.info("Recentering halo %d %f pc away." %
+                  (halo.quantities["particle_identifier"],
+                   distance.in_units("pc")))
+
+    set_halo_center(halo, new_center)
+    del sphere
+add_callback("field_max_center", field_max_center)

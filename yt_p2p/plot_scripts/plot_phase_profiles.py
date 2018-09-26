d54e6121d4e59d926a251df0b3c0158c53a14433
Britton Smith <brittonsmith@gmail.com>
2018-09-26 13:54:25 -0700

    Add H2 fraction and temperature plots.

--- yt_p2p/plot_scripts/plot_phase_profiles.py
+++ yt_p2p/plot_scripts/plot_phase_profiles.py
@@ -14,7 +14,10 @@ from yt.extensions.p2p.plots import \
 from grid_figure import GridFigure
 
 def plot_density_HD_fraction(data_dir, halo_id):
-    filename = os.path.join(data_dir, "HD_fraction_%06d.h5" % halo_id)
+    xfield = 'density'
+    yfield = 'HD_fraction'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     my_fig = GridFigure(
         1, 1, figsize=(6, 4.5),
@@ -40,7 +43,8 @@ def plot_density_HD_fraction(data_dir, halo_id):
     yminor = None
     ylabel = "f$_{\\rm HD}$"
 
-    output_filename = "figures/density_HD_fraction.pdf"
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
 
     make_phase_plot(
         my_fig, my_axes, filename,
@@ -50,7 +54,10 @@ def plot_density_HD_fraction(data_dir, halo_id):
         output_filename)
 
 def plot_density_H2_fraction(data_dir, halo_id):
-    filename = os.path.join(data_dir, "H2_fraction_%06d.h5" % halo_id)
+    xfield = 'density'
+    yfield = 'H2_fraction'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     fontsize = 14
     my_fig = GridFigure(
@@ -77,7 +84,8 @@ def plot_density_H2_fraction(data_dir, halo_id):
     yminor = None
     ylabel = "f$_{\\rm H_{2}}$"
 
-    output_filename = "figures/density_H2_fraction.pdf"
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
 
     make_phase_plot(
         my_fig, my_axes, filename,
@@ -87,7 +95,10 @@ def plot_density_H2_fraction(data_dir, halo_id):
         output_filename)
 
 def plot_density_HD_H2_ratio(data_dir, halo_id):
-    filename = os.path.join(data_dir, "HD_H2_ratio_%06d.h5" % halo_id)
+    xfield = 'density'
+    yfield = 'HD_H2_ratio'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     my_fig = GridFigure(
         1, 1, figsize=(6, 4.5),
@@ -113,7 +124,8 @@ def plot_density_HD_H2_ratio(data_dir, halo_id):
     yminor = None
     ylabel = "f$_{\\rm HD}$ / f$_{\\rm H_{2}}$"
 
-    output_filename = "figures/density_HD_H2_ratio.pdf"
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
 
     make_phase_plot(
         my_fig, my_axes, filename,
@@ -123,7 +135,10 @@ def plot_density_HD_H2_ratio(data_dir, halo_id):
         output_filename)
 
 def plot_density_temperature(data_dir, halo_id):
-    filename = os.path.join(data_dir, "temperature_%06d.h5" % halo_id)
+    xfield = 'density'
+    yfield = 'temperature'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     my_fig = GridFigure(
         1, 1, figsize=(6, 4.5),
@@ -149,7 +164,8 @@ def plot_density_temperature(data_dir, halo_id):
     yminor = None
     ylabel = "T [K]"
 
-    output_filename = "figures/density_temperature.pdf"
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
 
     make_phase_plot(
         my_fig, my_axes, filename,
@@ -159,7 +175,10 @@ def plot_density_temperature(data_dir, halo_id):
         output_filename)
 
 def plot_radius_density(data_dir, halo_id):
-    filename = os.path.join(data_dir, "density_%06d.h5" % halo_id)
+    xfield = 'radius'
+    yfield = 'density'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     fontsize = 14
     my_fig = GridFigure(
@@ -187,14 +206,60 @@ def plot_radius_density(data_dir, halo_id):
     ylim = (1e-25, 1e-10)
     ymajor = np.logspace(-25, -10, 6)
     yminor = np.logspace(-25, -10, 16)
+    ylabel = "$\\rho$ [g/cm$^{-3}$]"
     mirror_yticks(my_axes, ylim, ymajor, yminor=yminor)
     draw_major_grid(my_axes, 'y', ymajor)
-    my_axes.yaxis.set_label_text("$\\rho$ [g/cm$^{-3}$]")
+    my_axes.yaxis.set_label_text(ylabel)
 
-    pyplot.savefig("figures/radius_density.pdf")
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
+    pyplot.savefig(output_filename)
+
+def plot_radius_H2_fraction(data_dir, halo_id):
+    xfield = 'radius'
+    yfield = 'H2_fraction'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
+
+    fontsize = 14
+    my_fig = GridFigure(
+        1, 1, figsize=(6, 4.5),
+        top_buffer = 0.14, bottom_buffer = 0.13,
+        left_buffer = 0.16, right_buffer = 0.19)
+
+    my_axes = my_fig[0]
+    my_axes.set_xscale('log')
+    my_axes.set_yscale('log')
+
+    my_cax = my_fig.add_cax(my_axes, "right", buffer=0.02,
+                            length=0.95, width=0.04)
+    plot_phase(filename, 'cell_mass', 'Msun',
+               my_axes, my_cax=my_cax,
+               cmap=yt_colormaps['dusk'])
+
+    my_cax.yaxis.set_label_text("M [M$_{\\odot}$]")
+
+    xlim = (2e-6, 2e2)
+    tx = twin_unit_axes(
+        my_axes, xlim, "r",
+        "pc", top_units="AU")
+
+    ylim = (1e-8, 1)
+    ymajor = np.logspace(-8, 0, 9)
+    ylabel = "f$_{\\rm H_{2}}$"
+    draw_major_grid(my_axes, 'y', ymajor)
+    mirror_yticks(my_axes, ylim, ymajor)
+    my_axes.yaxis.set_label_text(ylabel)
+
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
+    pyplot.savefig(output_filename)
 
 def plot_radius_metallicity(data_dir, halo_id):
-    filename = os.path.join(data_dir, "metallicity3_min7_%06d.h5" % halo_id)
+    xfield = 'radius'
+    yfield = 'metallicity3_min7'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
 
     fontsize = 14
     my_fig = GridFigure(
@@ -221,11 +286,54 @@ def plot_radius_metallicity(data_dir, halo_id):
 
     ylim = (4.5e-8, 0.1)
     ymajor = np.logspace(-7, -1, 7)
+    ylabel = "Z [Z$_{\\odot}$]"
     mirror_yticks(my_axes, ylim, ymajor)
     draw_major_grid(my_axes, 'y', ymajor)
-    my_axes.yaxis.set_label_text("Z [Z$_{\\odot}$]")
+    my_axes.yaxis.set_label_text(ylabel)
 
-    pyplot.savefig("figures/radius_metallicity.pdf")
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
+    pyplot.savefig(output_filename)
+
+def plot_radius_temperature(data_dir, halo_id):
+    xfield = 'radius'
+    yfield = 'temperature'
+    filename = os.path.join(data_dir, "%s_%06d.h5" %
+                            (yfield, halo_id))
+
+    fontsize = 14
+    my_fig = GridFigure(
+        1, 1, figsize=(6, 4.5),
+        top_buffer = 0.14, bottom_buffer = 0.13,
+        left_buffer = 0.14, right_buffer = 0.19)
+
+    my_axes = my_fig[0]
+    my_axes.set_xscale('log')
+    my_axes.set_yscale('log')
+
+    my_cax = my_fig.add_cax(my_axes, "right", buffer=0.02,
+                            length=0.95, width=0.04)
+    plot_phase(filename, 'cell_mass', 'Msun',
+               my_axes, my_cax=my_cax,
+               cmap=yt_colormaps['dusk'])
+
+    my_cax.yaxis.set_label_text("M [M$_{\\odot}$]")
+
+    xlim = (2e-6, 2e2)
+    tx = twin_unit_axes(
+        my_axes, xlim, "r",
+        "pc", top_units="AU")
+
+    ylim = (10, 2e4)
+    ymajor = np.logspace(1, 4, 4)
+    ylabel = "T [K]"
+    mirror_yticks(my_axes, ylim, ymajor)
+    draw_major_grid(my_axes, 'y', ymajor)
+    my_axes.yaxis.set_label_text(ylabel)
+
+    output_filename = "figures/%s_%s.pdf" % \
+      (xfield, yfield)
+    pyplot.savefig(output_filename)
 
 if __name__ == "__main__":
     halo_id = 41732
@@ -243,4 +351,8 @@ if __name__ == "__main__":
     data_dir = "../halo_catalogs/profile_catalogs/DD0560/radial_profiles"
     plot_radius_density(data_dir, halo_id)
     pyplot.clf()
+    plot_radius_H2_fraction(data_dir, halo_id)
+    pyplot.clf()
     plot_radius_metallicity(data_dir, halo_id)
+    pyplot.clf()
+    plot_radius_temperature(data_dir, halo_id)

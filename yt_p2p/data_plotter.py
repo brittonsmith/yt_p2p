40328010ef19b10f7759224a25856eff01235d13
Britton Smith <brittonsmith@gmail.com>
2021-07-24 08:01:42 +0100

    Add more plotting options to DataPlotter.

--- yt_p2p/data_plotter.py
+++ yt_p2p/data_plotter.py
@@ -1,18 +1,29 @@
 from matplotlib import pyplot
 
 class DataPlotter:
-    def __init__(self, plots, labels):
+    def __init__(self, plots, labels, figsize=None, plot_func="loglog"):
         self.plots = plots
         self.labels = labels
+        self.plot_func = plot_func
+
         n_plots = len(plots)
-        self.fig, axl = pyplot.subplots(n_plots, 1, figsize=(15, n_plots * 8))
+        if figsize is None:
+            figsize = [15, 8]
+        elif isinstance(figsize, tuple):
+            figsize = list(figsize)
+        figsize[1] *= n_plots
+        self.fig, axl = pyplot.subplots(n_plots, 1, figsize=figsize)
         self.axes = {plot[0]: axes for plot, axes in zip(plots, axl)}
         
     def plot_data(self, data, color):
         for plot in self.plots:
-            y_data = data[plot[0]]
-            x_data = data[plot[1]]
-            self.axes[plot[0]].loglog(x_data, y_data, color=color)
+            y_field, x_field = plot
+            y_data = data.get(plot[0])
+            x_data = data.get(plot[1])
+            if None in (x_data, y_data):
+                continue
+            my_plot = getattr(self.axes[y_field], self.plot_func)
+            my_plot(x_data, y_data, color=color)
             
     def plot_axes(self):
         for plot in self.plots:

20b00e42d0b0a9a921695a82e36b1206e9bb8609
Britton Smith <brittonsmith@gmail.com>
2021-07-26 13:48:12 +0100

    Implement plot_datum.

--- yt_p2p/data_plotter.py
+++ yt_p2p/data_plotter.py
@@ -5,6 +5,7 @@ class DataPlotter:
         self.plots = plots
         self.labels = labels
         self.plot_func = plot_func
+        self.legends = []
 
         n_plots = len(plots)
         if figsize is None:
@@ -15,19 +16,26 @@ class DataPlotter:
         self.fig, axl = pyplot.subplots(n_plots, 1, figsize=figsize)
         self.axes = {plot[0]: axes for plot, axes in zip(plots, axl)}
         
-    def plot_data(self, data, color):
+    def plot_data(self, data, **pkwargs):
         for plot in self.plots:
             y_field, x_field = plot
             y_data = data.get(plot[0])
             x_data = data.get(plot[1])
             if None in (x_data, y_data):
                 continue
-            my_plot = getattr(self.axes[y_field], self.plot_func)
-            my_plot(x_data, y_data, color=color)
-            
+            self.plot_datum(y_field, x_data, y_data, **pkwargs)
+
+    def plot_datum(self, pfield, x_data, y_data, **pkwargs):
+        if "label" in pkwargs and pfield not in self.legends:
+            self.legends.append(pfield)
+        my_plot = getattr(self.axes[pfield], self.plot_func)
+        my_plot(x_data, y_data, **pkwargs)
+
     def plot_axes(self):
         for plot in self.plots:
             y_field, x_field = plot
             axes = self.axes[y_field]
             axes.xaxis.set_label_text(self.labels[x_field])
             axes.yaxis.set_label_text(self.labels[y_field])
+            if y_field in self.legends:
+                axes.legend(loc='best')

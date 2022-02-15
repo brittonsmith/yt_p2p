570f96c1262600943967d83518d14aa7db868904
Britton Smith <brittonsmith@gmail.com>
2022-02-15 14:49:47 +0000

    Update tree_icom scripts and remove redundant one.

--- scripts/tree_icom_all.py
+++ scripts/tree_icom_all.py
@@ -8,12 +8,27 @@ from ytree.analysis import \
     add_operation
 from ytree.utilities.parallel import parallel_nodes
 
-from yt.extensions.p2p.tree_analysis_operations import add_analysis_fields
+from yt.extensions.p2p.tree_analysis_operations import \
+    add_analysis_fields, \
+    fields_not_assigned, \
+    yt_dataset, \
+    node_icom, \
+    delattrs, \
+    garbage_collect
 
 def minimum_mass(node, val):
     return node["mass"] >= val
 
-add_operation("minimum_mass", minimum_mass)
+def in_region(node):
+    ds = node.ds
+    refine_left = ds.arr(ds.parameters['RefineRegionLeftEdge'], 'unitary')
+    refine_right = ds.arr(ds.parameters['RefineRegionRightEdge'], 'unitary')
+    center = reunit(ds, node['position'], 'unitary')
+    radius = reunit(ds, node['virial_radius'], 'unitary')
+
+    node["in_region"] = int((center - radius >= refine_left).all() and
+                            (center + radius <= refine_right).all())
+    return node["in_region"]
 
 if __name__ == "__main__":
     afn = sys.argv[1]
@@ -25,6 +40,7 @@ if __name__ == "__main__":
                'icom_all_position_x': {'units': 'unitary', 'default': -1},
                'icom_all_position_y': {'units': 'unitary', 'default': -1},
                'icom_all_position_z': {'units': 'unitary', 'default': -1}}
+    # afields['in_region'] = {'units': '', 'default': -1, 'dtype': np.int32}
     add_analysis_fields(a, afields)
 
     m_min = a.quan(1e3, 'Msun')
@@ -34,6 +50,7 @@ if __name__ == "__main__":
     data_dir = '.'
     ap = AnalysisPipeline()
     ap.add_operation("minimum_mass", m_min)
+    # ap.add_operation("in_region")
     ap.add_operation("fields_not_assigned", afields)
     ap.add_operation("yt_dataset", data_dir)
     ap.add_operation("node_icom")

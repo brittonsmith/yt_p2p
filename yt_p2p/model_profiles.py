6a415eb85196bbc38b4cd5d36fe7a8e8c651fa11
Britton Smith <brittonsmith@gmail.com>
2022-04-12 16:17:31 +0100

    Reorganize models and model_profiles.

--- yt_p2p/model_profiles.py
+++ yt_p2p/model_profiles.py
@@ -2,21 +2,219 @@
 Functions for preparing profile data for the Minihalo model.
 """
 
+from collections import defaultdict
 import numpy as np
 import os
-
 from unyt import uvstack
 
 import yt
-from yt.funcs import ensure_dir
+from yt.funcs import ensure_dir, get_pbar
+from yt.loaders import load as yt_load
+from yt.utilities.logger import ytLogger
 from yt.utilities.physical_constants import G
 
-from yt.extensions.p2p.models import \
-    find_peaks, \
-    rebin_profiles, \
-    get_datasets, \
-    load_model_profiles
+from ytree.utilities.logger import log_level
+
 from yt_p2p.stars import get_star_data
+
+def get_profile_datasets(fns):
+    pbar = get_pbar('Loading datasets', len(fns))
+    dss = []
+    for i, fn in enumerate(fns):
+        if fn is None:
+            ds = None
+        else:
+            ds = yt_load(fn, minimal_fields=True)
+        dss.append(ds)
+        pbar.update(i+1)
+    pbar.finish()
+    return dss
+
+_model_es = None
+_model_stars = None
+def load_model_profiles(star_id,
+                        data_dir="star_minihalos",
+                        stars_fn="star_hosts.yaml",
+                        sim_fn="simulation.h5"):
+
+    global _model_es
+    if _model_es is None:
+        _model_es = yt_load(sim_fn)
+    es = _model_es
+    times = es.data["time"].to("Myr")
+    fns = es.data["filename"].astype(str)
+
+    global _model_stars
+    if _model_stars is None:
+        _model_stars = get_star_data(stars_fn)
+    star_data = _model_stars
+
+    ct = star_data[star_id]["creation_time"]
+
+    dsfns = defaultdict(list)
+    weights = ["None", "cell_volume", "cell_mass"]
+
+    for t, fn in zip(times, fns):
+        if t <= ct:
+            pdir = "icom_gas_position"
+            to_get = ["None", "cell_volume", "cell_mass"]
+        else:
+            pdir = "icom_all_position"
+            to_get = ["None", "cell_volume"]
+
+        exists = 0
+        these_fns = {}
+        for weight in weights:
+            if weight in to_get:
+                basename = f"{os.path.basename(fn)}_profile_weight_field_{weight}.h5"
+                my_fn = os.path.join(
+                    data_dir, f"star_{star_id}", "profiles", pdir, basename)
+                if os.path.exists(my_fn):
+                    exists += 1
+            else:
+                my_fn = None
+
+            these_fns[weight] = my_fn
+
+        if len(to_get) == exists:
+            for weight in weights:
+                dsfns[weight].append(these_fns[weight])
+
+    # dict of lists
+    with log_level(40, mylog=ytLogger):
+        my_dss = {weight: get_profile_datasets(dsfns[weight]) for weight in weights}
+    # list of dicts
+    rval = [{weight: my_dss[weight][i] for weight in weights}
+            for i in range(len(my_dss[weights[0]]))]
+
+    return rval
+
+# Profile rebinning functions
+
+def rebin_profile(profile, bin_field, new_bins):
+    """
+    Rebin profile data using linear interpolation.
+    """
+    ibins = np.digitize(new_bins, profile[bin_field]) - 1
+    do = (ibins >= 0) & (ibins < profile[bin_field].size - 1)
+
+    l_new_bins = np.log10(new_bins)
+    l_old_bins = np.log10(profile[bin_field])
+    new_profile = {bin_field: profile[bin_field].units * new_bins}
+    for field, data in profile.items():
+        if field in [bin_field, ('data', 'used')]:
+            continue
+
+        new_data = data.units * np.zeros(new_bins.size)
+        new_profile[field] = new_data
+        if data.nonzero()[0].size == 0:
+            continue
+
+        l_data = np.log10(data.clip(1e-100, np.inf))
+        slope = (l_data[ibins[do]+1] - l_data[ibins[do]]) / \
+            (l_old_bins[ibins[do]+1] - l_old_bins[ibins[do]])
+        new_l_data = slope * (l_new_bins[do] - l_old_bins[ibins[do]]) + \
+          l_data[ibins[do]]
+        new_data[do] = np.power(10, new_l_data)
+
+    return new_profile
+
+def rebin_profiles(profile_data, bin_field, bin_density):
+    """
+    Rebin a list of profiles with a different field.
+    """
+    bmin, bmax = calc_global_binning(profile_data, bin_field)
+    n_bins = int(np.round((bmax - bmin) * bin_density)) + 1
+    p_bins = np.logspace(bmin, bmax, n_bins, endpoint=True)
+
+    new_profiles = []
+    for profile in profile_data:
+        new_profiles.append(rebin_profile(profile, bin_field, p_bins))
+
+    return new_profiles
+
+def default_func(func, tracked_val, new_vals):
+    if tracked_val is None:
+        return func(new_vals)
+    else:
+        return func(tracked_val, func(new_vals))
+
+def track_global_binning(pdata, bin_data):
+    if not bin_data:
+        for key in ['max', 'min']:
+            bin_data[key] = None
+
+    bin_data['min'] = default_func(min, bin_data['min'], pdata)
+    bin_data['max'] = default_func(max, bin_data['max'], pdata)
+
+def calc_global_binning(data, field, log=True, rounding=True, nonzero=True):
+    binning = {}
+    for datum in data:
+
+        my_datum = datum[field]
+        if nonzero:
+            my_datum = my_datum[my_datum.nonzero()[0]]
+        if log:
+            my_datum = np.log10(my_datum)
+
+        track_global_binning(my_datum, binning)
+    if rounding:
+        binning['min'] = np.floor(binning['min'])
+        binning['max'] = np.ceil(binning['max'])
+    return binning['min'], binning['max']
+
+# Peak finding functions
+
+def udwshed(x, within=5):
+    """
+    Upside-down watershed. Use a watershed algorithm to find local maxima.
+    """
+    asx = np.flip(x.argsort())
+    basins = np.full(x.size, -1, dtype=np.int8)
+    nb = 0
+    for i in asx:
+        for ib in range(nb):
+            diff = np.abs(i - np.where(basins == ib)[0]).min()
+            if diff > within:
+                continue
+            basins[i] = ib
+            break
+        if basins[i] < 0:
+            basins[i] = nb
+            nb += 1
+    return basins
+
+def basin_peak_ids(x):
+    basin = udwshed(x)
+    peaks = []
+    for i in range(basin.max()+1):
+        ib = np.where(basin == i)[0]
+        peaks.append(ib[x[ib].argmax()])
+    return np.array(peaks)
+
+def find_peaks(ds, bin_field, peak_field, time_index):
+    peak_data = ds.data[peak_field][time_index]
+    bin_data = ds.data[bin_field][time_index]
+
+    peak_used = peak_data > 0
+    peak_data = peak_data[peak_used]
+    bin_data = bin_data[peak_used]
+
+    i_peaks = basin_peak_ids(peak_data)
+    i_peaks = i_peaks[bin_data[i_peaks] < 1e4]
+
+    # if peak is above 1, include the innermost coordinate that is above 1
+    if peak_data.max() > 1:
+        i_peaks = np.append(i_peaks, np.where(peak_data > 1)[0].min())
+
+    if i_peaks.size == 0:
+        i_peaks = np.append(i_peaks, np.argmax(peak_data))
+
+    # the actual indices in the profile_all arrays
+    i_peaks = np.where(peak_used)[0][i_peaks]
+    i_peaks = np.unique(i_peaks)
+    i_peaks.sort()
+    return i_peaks
 
 def create_profile_cube(star_id, output_dir="star_cubes"):
     ### Bonnor-Ebert Mass constant

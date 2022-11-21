15a8c19ee1f8bfdfd1162b559a8824e6ff610669
Britton Smith <brittonsmith@gmail.com>
2022-11-21 15:54:27 +0000

    Update metallicity model script to plot grid of metallicities and use halo mass as twinx.

--- plot_scripts/plot_metallicity_model.py
+++ plot_scripts/plot_metallicity_model.py
@@ -1,9 +1,15 @@
-from matplotlib import pyplot
+import matplotlib as mpl
+from matplotlib import pyplot, colors
 from matplotlib.ticker import FuncFormatter
 import numpy as np
 import os
+from scipy.interpolate import interp1d
 import sys
+import yaml
 import yt
+import ytree
+from ytree.data_structures.tree_container import TreeContainer
+from yt.extensions.p2p.tree_analysis_operations import get_progenitor_line
 
 pyplot.rcParams['font.size'] = 14
 
@@ -32,6 +38,9 @@ def _log_to_exp(val, decimals=1):
         return "10$^{%d}$" % exp
 
 if __name__ == "__main__":
+    with open("models.yaml", 'r') as f:
+        models = yaml.load(f, Loader=yaml.FullLoader)
+
     data_dir = "minihalo_models/metallicity_grids"
     star_data = get_star_data("star_hosts.yaml")
     star_ids = list(star_data.keys())
@@ -41,6 +50,19 @@ if __name__ == "__main__":
     else:
         model_id = 1
     star_id = star_ids[model_id-1]
+
+    my_model = models.get(star_id, {})
+    my_solutions = my_model.get("solutions", {})
+
+    tolerance = 1e-3
+    my_tol = f"{tolerance:g}"
+    if my_tol in my_solutions:
+        solution_fn = my_solutions[my_tol]["filename"]
+        solution_value = my_solutions[my_tol]["value"]
+        print (f"Solution ({my_tol}): {solution_value} - {solution_fn}")
+    else:
+        solution_fn = ""
+        solution_value = None
 
     star_dir = os.path.join(data_dir, f"star_{star_id}")
     co = Cosmology(
@@ -53,7 +75,7 @@ if __name__ == "__main__":
     creation_time = my_star["creation_time"]
 
     my_fig = GridFigure(3, 1, figsize=(8, 6),
-                    left_buffer=0.15, right_buffer=0.02,
+                    left_buffer=0.15, right_buffer=0.2,
                     bottom_buffer=0.1, top_buffer=0.09,
                     vertical_buffer=0, horizontal_buffer=0.12)
 
@@ -72,10 +94,15 @@ if __name__ == "__main__":
 
     for my_axes in my_fig.bottom_axes:
         my_axes.set_xlabel("t [Myr]", labelpad=3)
+        my_axes.tick_params(axis="x", direction="in", which="minor",
+                            labelbottom=True, pad=-12,
+                            top=False, bottom=True)
 
     mcolor = "blue"
 
-    log_metallicities = [-np.inf, -4, -3.5, -3]
+    # log_metallicities = [-np.inf, -4, -3.5, -3]
+    # log_metallicities = np.concatenate([[-np.inf], np.linspace(-5, -3, 21)])
+    log_metallicities = np.linspace(-5, -3, 21)
 
     dss = {}
     min_t = None
@@ -96,8 +123,8 @@ if __name__ == "__main__":
     xlim = (np.floor(min_t/10)*10, np.ceil(max_t/10)*10)
     for i, lZ in enumerate(log_metallicities):
         model_ds = dss[lZ]
-        # color = pyplot.cm.turbo(i / (len(log_metallicities)-1))
-        color = pyplot.cm.turbo(i / 6)
+        color = pyplot.cm.turbo(i / (len(log_metallicities)-1))
+        # color = pyplot.cm.turbo(i / 6)
 
         model_data = model_ds.data
         model_time = model_data["data", "time"].to("Myr") + \
@@ -129,7 +156,7 @@ if __name__ == "__main__":
     my_fig[0].yaxis.set_label_text("$\\rho_{\\rm gas}$ [g/cm$^{3}$]")
     my_fig[0].yaxis.set_ticks(np.logspace(-23, -19, 3))
     my_fig[0].yaxis.set_ticks(np.logspace(-22, -20, 2), minor=True, labels="")
-    my_fig[0].legend(markerfirst=False, fontsize=10)
+    # my_fig[0].legend(markerfirst=False, fontsize=14)
     my_fig[1].yaxis.set_label_text("T [K]")
     # my_fig[1].yaxis.set_ticks([50, 100, 200, 400])
     my_fig[1].yaxis.set_ticks([100, 1000])
@@ -138,22 +165,94 @@ if __name__ == "__main__":
     my_fig[2].yaxis.set_label_text("M$_{\\rm gas, enc}$ / M$_{\\rm BE}$")
     my_fig[2].yaxis.set_ticks(np.logspace(-4, 0, 3))
     my_fig[2].yaxis.set_ticks(np.logspace(-3, -1, 2), minor=True, labels="")
+    my_fig[2].xaxis.set_ticks([my_star["creation_time"].d], labels="*", minor=True)
+
+    Z_min = log_metallicities[0]
+    Z_max = log_metallicities[-1]
+    norm = colors.Normalize(vmin=Z_min, vmax=Z_max)
+    # Z_vals = np.linspace(Z_min, Z_max, 41)
+    cmap = "turbo"
+    my_cax = my_fig.add_cax(my_fig[1], "right", length=2.95)
+    cbar = mpl.colorbar.ColorbarBase(
+        my_cax, cmap=cmap, boundaries=log_metallicities,
+        norm=norm, orientation='vertical')
+
+    cbar.set_label("log$_{\\rm 10}$ (Z [Z$_{\\odot}$])")
+    my_ticks = list(np.linspace(-5, -3, 9))
+    # my_ticks = list(np.linspace(-5, -3, 5))
+    cbar.set_ticks(my_ticks)
+    cbar.solids.set_edgecolor("face")
+    if solution_value is not None:
+        cbar.set_ticks([solution_value], minor=True)
+    my_cax.axhline(solution_value, color="black")
 
     for my_axes in my_fig:
         my_axes.set_xlim(*xlim)
+
+    # my_axes = my_fig[0]
+    # tx = my_axes.twiny()
+    # tx.xaxis.tick_top()
+    # z_ticks_in_t = co.t_from_z(
+    #     np.array([25, 20, 15, 12, 10, 9])).in_units("Myr")
+    # tx.xaxis.set_ticks(z_ticks_in_t.d)
+    # z_ticks_in_t_m = co.t_from_z(
+    #     np.arange(9, 33)).in_units("Myr")
+    # tx.xaxis.set_ticks(z_ticks_in_t_m.d, minor=True, labels="")
+    # tx.xaxis.set_major_formatter(FuncFormatter(_z_from_t))
+    # tx.set_xlim(*xlim)
+    # tx.xaxis.set_label_text("z")
+
+    ### arbor stuff
+    a = ytree.load(my_star["arbor"])
+    my_root = a[my_star["_arbor_index"]]
+    my_tree = my_root.get_node("forest", my_star["tree_id"])
+    ct = my_star["creation_time"]
+    prog = TreeContainer(a, get_progenitor_line(my_tree))
+    good = (prog["time"] <= ct) | ((prog["time"] > ct) & (prog["mass"] > 1e4))
+
+
+    my_x1 = prog["time"][good].to("Myr").d
+    my_y1 = np.log10(prog["mass"][good].to("Msun").d)
+    f1 = interp1d(my_x1, my_y1)
+
+    i1 = np.where(good)[0][-10]
+    i2 = np.where(good)[0][-1]
+    my_x2 = np.array([prog["time"][i1], prog["time"][i2]])
+    my_y2 = np.log10([prog["mass"][i1], prog["mass"][i2]])
+    ikwargs = {"kind": "linear", "fill_value": "extrapolate"}
+    f2 = interp1d(my_x2, my_y2, **ikwargs)
+
+    def _m_from_t(t, pos):
+        if t <= my_x1[-1]:
+            my_f = f1
+        else:
+            my_f = f2
+        val = my_f(t)
+        if np.abs(np.round(val) - val) < 1e-2:
+            val = np.round(val)
+        dec = 10**(val - int(val))
+        if dec > 2:
+            return "%dx10$^{%d}$" % (int(np.round(dec)), my_f(t))
+        if dec > 1.1:
+            return ""
+        return "10$^{%d}$" % val
+
+    f1r = interp1d(my_y1, my_x1)
+    f2r = interp1d(my_y2, my_x2, **ikwargs)
+
+    m_ticks = np.arange(5, int(my_y1.max()) + 1)
+    m_ticks_in_t = f1r(m_ticks)
+    # if my_y1[-1] > m_ticks.max():
+    #     m_ticks_in_t = np.append(m_ticks_in_t, my_x1[-1])
+    # m_ticks_in_t = np.append(m_ticks_in_t, f2r(7))
 
     my_axes = my_fig[0]
     tx = my_axes.twiny()
     tx.xaxis.tick_top()
 
-    z_ticks_in_t = co.t_from_z(
-        np.array([25, 20, 15, 12, 10, 9])).in_units("Myr")
-    tx.xaxis.set_ticks(z_ticks_in_t.d)
-    z_ticks_in_t_m = co.t_from_z(
-        np.arange(9, 33)).in_units("Myr")
-    tx.xaxis.set_ticks(z_ticks_in_t_m.d, minor=True, labels="")
-    tx.xaxis.set_major_formatter(FuncFormatter(_z_from_t))
+    tx.xaxis.set_ticks(m_ticks_in_t)
+    tx.xaxis.set_major_formatter(FuncFormatter(_m_from_t))
     tx.set_xlim(*xlim)
-    tx.xaxis.set_label_text("z")
+    tx.xaxis.set_label_text("M$_{\\rm halo}$ [M$_{\\odot}$]")
 
     pyplot.savefig(f"metallicity_model_{model_id}.pdf")
